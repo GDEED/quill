e6964a32840aa7208a6bfd33fc233bbf1ef7f89d
adias9 <andreasdias9@gmail.com>
2018-12-21 16:18:40 -0600

    potential bug fix

--- app/server/routes.js
+++ app/server/routes.js
@@ -3,9 +3,11 @@ var User = require('./models/User');
 module.exports = function(app) {
 
   // Application ------------------------------------------
-  app.get('/*', function(req, res){
-    res.sendFile('/app/client/index.html', {"root": '.'});
-  });
+  // app.get('/*', function(req, res){
+  //   res.sendFile('/app/client/index.html', {"root": '.'});
+  // });
+
+  app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
   // Wildcard all other GET requests to the angular app
   // app.get('*', function(req, res){
